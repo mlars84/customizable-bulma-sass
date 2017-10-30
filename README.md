f415fafd6931d192d9c50918d1716884aac3579c
Matt Larson <matt.a.larson@gmail.com>
2017-10-30 11:09:41 -0500

    added comments to main.scss

--- README.md
+++ README.md
@@ -13,3 +13,6 @@
 - Run 
 - $ `sass public/styles/main.scss --watch` 
   - to continue editing and watch for changes as you go.
+
+  ## A note on HSL colors
+  - Bulma's colors are defined with HSL (Hue, Saturation, Light) as opposed to hex or rgb values. Hue is a degree on the color wheel from 0 to 360. 0 is red, 120 is green, 240 is blue. Values are specified with: `hsl(hue, saturation, lightness)`. [Here](https://www.w3schools.com/colors/colors_hsl.asp) is an HSL calculator.
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, './public')));
 
 app.listen(PORT, function(req, res) {
-  console.log(`Listening on ${PORT}`)
-})
+  console.log(`Listening on ${PORT}`);
+});
